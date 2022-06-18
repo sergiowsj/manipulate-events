5cb3e3012ed7bea2ec09644291d381c0d553ddca
sergiowsj <85712143+sergiowsj@users.noreply.github.com>
2022-06-18 12:59:00 -0300

    Add files via upload

---
+++ 2manipulate-events.js
@@ -0,0 +1,11 @@
+function doChange() {
+  alert(event.target.value)
+}
+
+let sampleInput = document.querySelector('input[name="sample-input"]')
+
+sampleInput.addEventListener('change', doChange)
+
+let changeEvent = new Event('change')
+
+sampleInput.dispatchEvent(changeEvent)
